8d4f772c6a53f5363011eef357bec22d03478251
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-10-20 16:03:50 +0300

    Create OnBoardingScreen UI

---
+++ lib/ui/screen/onboarding/onboarding_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:linky/ui/screen/register/register_screen.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Linky',
+              style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue),
+            ),
+            const SizedBox(height: 32),
+            const Text(
+              'Linky is Your personal connection hub,'
+              ' \n where you can easily chat, share links,'
+              ' \n and stay connected with friends and family.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RegisterScreen()));
+              },
+              child: const Text('Get Started'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
