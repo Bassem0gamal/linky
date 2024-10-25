492acf17d06fb4701e1b508d0c68a3421f750c5f
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-10-25 16:28:05 +0300

    Add FireBase Auth Logic to RegisterScreen Add Validation for the TextField in RegisterScreen Add some Improvement

--- lib/ui/screen/register/register_screen.dart
+++ lib/ui/screen/register/register_screen.dart
@@ -15,6 +15,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
   bool _obscureTextP = true;
   bool _obscureTextPC = true;
 
+  final _formKey = GlobalKey<FormState>();
+
+  final _userNameTextController = TextEditingController();
   final _emailTextController = TextEditingController();
   final _passwordTextController = TextEditingController();
   final _confirmPasswordTextController = TextEditingController();
@@ -22,106 +25,200 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Linky',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,color: Colors.blue
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text('Register and let’s chat with Linky'),
-              const SizedBox(height: 8),
-              TextField(
-                controller: _emailTextController,
-                decoration: InputDecoration(
-                  labelText: 'User name',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Linky',
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue),
                   ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _passwordTextController,
-                obscureText: _obscureTextP,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(_obscureTextP ?Icons.visibility : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _obscureTextP = !_obscureTextP;
-                      });
+                  const SizedBox(height: 20),
+                  const Text('Register and let’s chat with Linky'),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
                     },
+                    controller: _userNameTextController,
+                    decoration: InputDecoration(
+                      labelText: 'User name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
                   ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _confirmPasswordTextController,
-                obscureText: _obscureTextPC,
-                decoration: InputDecoration(
-                  labelText: 'Confirm Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(_obscureTextPC ?Icons.visibility : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _obscureTextPC = !_obscureTextPC;
-                      });
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
                     },
+                    controller: _emailTextController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
                   ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                    controller: _passwordTextController,
+                    obscureText: _obscureTextP,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(_obscureTextP
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _obscureTextP = !_obscureTextP;
+                          });
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please confirm your password';
+                      } else if (value != _passwordTextController.text) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                    controller: _confirmPasswordTextController,
+                    obscureText: _obscureTextPC,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(_obscureTextPC
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _obscureTextPC = !_obscureTextPC;
+                          });
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        try {
+                          const CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation(Colors.blue),
+                          );
+                          await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                            email: _emailTextController.text,
+                            password: _passwordTextController.text,
+                          );
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomeScreen()));
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Registration successful!'),
+                              backgroundColor: Colors.green,
+                            ),
+                          );
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            print('The password provided is too weak.');
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('The password provided is too weak.'),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          } else if (e.code == 'email-already-in-use') {
+                            print('The account already exists for that email.');
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text(
+                                    'The account already exists for that email.'),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          }
+                        } catch (e) {
+                          print(e);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('$e'),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Divider(
+                    color: Colors.grey,
+                    thickness: .5,
+                  ),
+                  const Text(
+                    'If you already registered \n please Sign-in',
+                    textAlign: TextAlign.center,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignInScreen()));
+                    },
+                    child: const Text(
+                      'Sign-in',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // TODO :Handle registration logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                ),
-                child: const Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Divider(
-                color: Colors.grey,
-                thickness: .5,
-              ),
-              const Text(
-                'If you already registered \n please Sign-in',
-                textAlign: TextAlign.center,
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SignInScreen()));
-                },
-                child: const Text(
-                  'Sign-in',
-                  style: TextStyle(color: Colors.blue),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ]
       ),
     );
   }
