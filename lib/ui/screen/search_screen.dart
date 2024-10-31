b7471714f443e1a886739a5e110bde72c787ad47
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-10-31 18:01:58 +0300

    add search & Nav Function to SearchScreen

--- lib/ui/screen/search_screen.dart
+++ lib/ui/screen/search_screen.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:linky/ui/screen/chat_screen.dart';
+import 'package:linky/ui/data_source/users_name_service.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -10,7 +13,10 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  List _allUsers = [];
+  final user = FirebaseAuth.instance.currentUser;
+  final userNameService = UserNamesService();
+
+  List<QueryDocumentSnapshot<dynamic>> _allUsers = [];
   List _searchFilter = [];
 
   @override
@@ -50,8 +56,11 @@ class _SearchScreenState extends State<SearchScreen> {
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(
-              _searchFilter[index]["user_name"],
+              _searchFilter[index]["name"],
             ),
+            onTap: () {
+              _navigateToChat(_searchFilter[index].id, );
+            },
           );
         },
       ),
@@ -59,21 +68,23 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   getUsers() async {
-    var data = await db.collection('users').orderBy('user_name').get();
+    var data = await db.collection('users').orderBy('name').get();
 
     setState(() {
-      _allUsers = data.docs;
+      _allUsers = data.docs.where((value) => user!.uid != value.id).toList();
     });
     _searchFilterResult();
   }
 
   _searchFilterResult() {
     List result = [];
-    if(_searchController.text != "") {
-      for(var snapshot in _allUsers) {
-        var name = snapshot['user_name'].toString().toLowerCase();
+    if (_searchController.text != "") {
+      for (var snapshot in _allUsers) {
+        var myId = user!.uid;
+        var userId = snapshot.id;
+        var name = snapshot['name'].toString().toLowerCase();
 
-        if(name.contains(_searchController.text.toLowerCase())) {
+        if (name.contains(_searchController.text.toLowerCase()) && userId != myId) {
           result.add(snapshot);
         }
       }
@@ -83,5 +94,55 @@ class _SearchScreenState extends State<SearchScreen> {
     setState(() {
       _searchFilter = result;
     });
+  }
+
+  Future<void> _navigateToChat(String otherUserId) async {
+    final currentUserId = FirebaseAuth.instance.currentUser!.uid;
+    final chatRoomId = getChatRoomId(currentUserId, otherUserId);
+
+    final chatRoomDoc = await FirebaseFirestore.instance
+        .collection('chat_rooms')
+        .doc(chatRoomId)
+        .get();
+
+    if (chatRoomDoc.exists) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatScreen(
+            roomId: chatRoomId,
+            otherUserId: otherUserId,
+            lastMessage: (chatRoomDoc['last_message_time'] as Timestamp).toDate(),
+          ),
+        ),
+      );
+    } else {
+      await FirebaseFirestore.instance
+          .collection('chat_rooms')
+          .doc(chatRoomId)
+          .set({
+        'members': [currentUserId, otherUserId],
+        'last_message_time': FieldValue.serverTimestamp(),
+      });
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatScreen(
+            roomId: chatRoomId,
+            otherUserId: otherUserId,
+            lastMessage: DateTime.now(),
+          ),
+        ),
+      );
+    }
+  }
+
+  String getChatRoomId(String userId1, String userId2) {
+    if (userId1.compareTo(userId2) < 0) {
+      return '$userId1$userId2';
+    } else {
+      return '$userId2$userId1';
+    }
   }
 }
