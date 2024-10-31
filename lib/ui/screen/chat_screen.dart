b3b22f65d6a1b51b578b6f2cb6c7a5901ff46046
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-10-31 18:02:24 +0300

    Fixing errors

--- lib/ui/screen/chat_screen.dart
+++ lib/ui/screen/chat_screen.dart
@@ -2,15 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:linky/ui/widget/chat_header.dart';
+import 'package:linky/ui/data_source/last_seen_service.dart';
+import 'package:linky/ui/data_source/users_name_service.dart';
 import 'package:linky/ui/widget/chat_widget.dart';
 
 class ChatScreen extends StatefulWidget {
   final String roomId;
+  final DateTime lastMessage;
+  final String otherUserId;
 
   const ChatScreen({
     super.key,
     required this.roomId,
+    required this.otherUserId,
+    required this.lastMessage,
   });
 
   @override
@@ -20,25 +25,35 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final _messageController = TextEditingController();
 
+  final _chatroomLastSeen = ChatRoomLastSeen();
+
   final user = FirebaseAuth.instance.currentUser;
   final db = FirebaseFirestore.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _chatroomLastSeen.setLastMessage(widget.roomId, widget.lastMessage);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
             },
             icon: const Icon(CupertinoIcons.back)),
-        title: const Center(
-          child: Padding(
-            padding: EdgeInsets.all(70.0),
-            child: ChatHeader(
-              name: 'None',
-            ),
-          ),
+        title: FutureBuilder(
+          future: UserNamesService().getUserName(widget.otherUserId),
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            return Text(
+              snapshot.data ?? 'Loading..',
+              style: const TextStyle(fontSize: 18),
+            );
+          },
         ),
         backgroundColor: Colors.blue,
       ),
@@ -48,28 +63,27 @@ class _ChatScreenState extends State<ChatScreen> {
             child: StreamBuilder(
                 stream: FirebaseFirestore.instance
                     .collection('messages')
-                    .where("room_id", isEqualTo: widget.roomId).orderBy("time_stamp",descending: true)
+                    .where("room_id", isEqualTo: widget.roomId)
+                    .orderBy("time_stamp", descending: true)
                     .snapshots(),
                 builder: (context, snapshot) {
-
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return const Center(child: CircularProgressIndicator());
                   }
 
-                  return
-                      ListView(
-                        reverse: true,
-                          children: snapshot.data!.docs.map((document) {
+                  return ListView(
+                      reverse: true,
+                      children: snapshot.data!.docs.map((document) {
                         final map = document.data();
 
                         return ChatWidget(
-                          name: 'senderName["user_name"]',
                           text: map["text"],
-                          date: (document.get("time_stamp") as Timestamp).toDate(),
+                          date: (document.get("time_stamp") as Timestamp?)
+                              ?.toDate() ??
+                              DateTime.now(),
                           isMe: user!.uid == map["sender_id"],
                         );
-
-                          }).toList());
+                      }).toList());
                 }),
           ),
           Container(
@@ -80,21 +94,23 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: [
                   Expanded(
                     child: TextFormField(
-                      
                       controller: _messageController,
                       decoration: const InputDecoration(
                         hintText: 'Type your message...',
-                        hintStyle: TextStyle(color: Colors.white)
+                        hintStyle: TextStyle(color: Colors.white),
                       ),
                     ),
                   ),
                   IconButton(
                       onPressed: () {
-                        _onMessageSend(_messageController.text, widget.roomId, user);
+                        _onMessageSend(
+                            _messageController.text, widget.roomId, user);
                         _messageController.clear();
                       },
-                      icon: const Icon(Icons.send,color: Colors.white,)
-                  ),
+                      icon: const Icon(
+                        Icons.send,
+                        color: Colors.white,
+                      )),
                 ],
               ),
             ),
@@ -105,19 +121,27 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
-_onMessageSend(String messageText, String roomId ,user) async {
-    const CircularProgressIndicator(
-      valueColor: AlwaysStoppedAnimation(Colors.blue),
-    );
+_onMessageSend(String messageText, String roomId, user) async {
+  const CircularProgressIndicator(
+    valueColor: AlwaysStoppedAnimation(Colors.blue),
+  );
 
-    if(messageText.isNotEmpty) {
-      final message = {
-        'room_id': roomId,
-        'sender_id': user!.uid,
-        'text': messageText,
-        'time_stamp': FieldValue.serverTimestamp(),
-      };
+  if (messageText.isNotEmpty) {
+    final message = {
+      'room_id': roomId,
+      'sender_id': user!.uid,
+      'text': messageText,
+      'time_stamp': FieldValue.serverTimestamp(),
+    };
 
-      await FirebaseFirestore.instance.collection("messages").add(message);
-    }
+    final chatRoomPayLoad = {
+      'time_stamp': FieldValue.serverTimestamp()
+    };
+
+    await FirebaseFirestore.instance.collection("messages").add(message);
+
+    await FirebaseFirestore.instance.collection('chat_rooms')
+        .doc(roomId)
+        .update(chatRoomPayLoad);
+  }
 }
